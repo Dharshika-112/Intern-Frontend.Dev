daf67e28ee0a3df25846ac09cb4ed1f5eaa42081
Dharshika <dsdharshika5@gmail.com>
2025-06-21 15:44:01 +0530

    Files are Uploaded

---
+++ Internship Task/Day 12/Task 6.js
@@ -0,0 +1,8 @@
+let a= prompt("Enter a Number");
+if(a%2==0){
+    alert("The Number is Even");
+    
+}
+else{
+    alert("The number isOdd");
+}
---
+++ Internship Task/Day 12/Task 7.js
@@ -0,0 +1,5 @@
+ let p= prompt("Enter a Number");
+ let r= prompt("Enter a Another number");
+ let t= prompt("Enter a Another number");
+ result=(p*r*t)/100;
+ alert("Simple interest is " + result)
